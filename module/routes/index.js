b65dbba6fc379aae347fbb71818e9a1d82584967
9ostrd <yoschanin.s@gmail.com>
2016-02-13 15:55:00 +0700

    update database

--- module/routes/index.js
+++ module/routes/index.js
@@ -1,13 +1,17 @@
-module.exports = function(app, db, config){
-    var fs = require('fs');
+module.exports = function(app, db, config) {
+  var fs = require('fs');
 
-    function ensureAuthorized(req, res, next){
+  function ensureAuthorized(req, res, next) {
+    if (res.locals.user) {
       next();
+    } else {
+      res.send(403);
     }
+  }
 
-    fs.readdirSync(__dirname).forEach(function(file) {
-        if (file == "index.js") return;
-        var name = file.substr(0, file.indexOf('.'));
-        require('./' + name)(app, ensureAuthorized, db, config);
-    });
+  fs.readdirSync(__dirname).forEach(function(file) {
+    if (file == "index.js") return;
+    var name = file.substr(0, file.indexOf('.'));
+    require('./' + name)(app, ensureAuthorized, db, config);
+  });
 }
