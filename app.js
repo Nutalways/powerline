43135afd16c3180131202e179fc3d8f787fe6146
Nutalways <nut_thanatcha@hotmail.co.th>
2016-02-13 21:11:36 +0700

    new update
    add views

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(morgan('dev'));
 //config cookieParser
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use(express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
 
 // include routes
 require('./module/routes')(app, db, config);
--- module/routes/auth.js
+++ module/routes/auth.js
@@ -1,7 +1,38 @@
 module.exports = function(app, ensureAuthorized, db, config) {
 
     app.get('/login', ensureAuthorized,  function(req, res) {
-        res.render('user/login', { title: '9ostrd' });
+        res.render('user/login', { title: 'login' });
+    });
+
+    app.get('/register', ensureAuthorized,  function(req, res) {
+        res.render('user/register', { title: 'register' });
+    });
+
+    app.get('/forget', ensureAuthorized,  function(req, res) {
+        res.render('user/forget', { title: 'forget' });
+    });
+
+    app.get('/home', ensureAuthorized,  function(req, res) {
+        res.render('user/home', { title: 'home' });
+    });
+
+    app.get('/home/time', ensureAuthorized,  function(req, res) {
+        res.render('user/time', { title: 'time' });
+    });
+
+    app.get('/home/device', ensureAuthorized,  function(req, res) {
+        res.render('user/device', { title: 'device' });
+    });
+    app.get('/home/time/add', ensureAuthorized,  function(req, res) {
+        res.render('user/addtime', { title: 'addtime' });
+    });
+
+    app.get('/home/time/edit', ensureAuthorized,  function(req, res) {
+        res.render('user/edittime', { title: 'edittime' });
+    });
+
+    app.get('/profile', ensureAuthorized,  function(req, res) {
+        res.render('user/profile', { title: 'profile' });
     });
 
 };
