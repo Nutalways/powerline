090e3538e6bbf8ebb1f470bebeeb42774a979534
Nutalways <nut_thanatcha@hotmail.co.th>
2016-03-23 20:19:34 +0700

    update config file

--- module/serial/serial.js
+++ module/serial/serial.js
@@ -5,7 +5,7 @@ module.exports = function (db, serial, config) {
 
 	method['send'] = function (deid, command) {
 		var deviceObj = db.device.getById(deid);
-		if (deviceObj) {
+		if (deviceObj && config.command[command] && config.housecode[deviceObj.housecode] && config.keycode[deviceObj.keycode]) {
 			serialPort.write(deviceObj.housecode + " " + deviceObj.keycode + " " + config.command[command]);
 			console.log(moment.utc().toDate().getTime(), "=====>", deviceObj.id, deviceObj.housecode, deviceObj.keycode, command);
 		}
