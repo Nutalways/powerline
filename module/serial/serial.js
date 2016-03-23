afa9a873aad8679e0360cd2bb5f09c97a1f30b80
Nutalways <nut_thanatcha@hotmail.co.th>
2016-03-23 22:09:53 +0700

    update serial
    update serial

--- module/serial/serial.js
+++ module/serial/serial.js
@@ -7,24 +7,33 @@ module.exports = function (db, config) {
 	method['send'] = function (deid, command) {
 		var deviceObj = db.device.getById(deid);
 		if (deviceObj && config.portName != "" && config.command[command] && config.housecode[deviceObj.housecode] && config.keycode[deviceObj.keycode]) {
-			console.log(moment.utc().toDate().getTime(), "=====>", "start send", deviceObj.id, config.housecode[deviceObj.housecode], config.keycode[deviceObj.keycode], command);
+			console.log(moment.utc().toDate().getTime(), "=====>", "start send", deviceObj.id, config.housecode[deviceObj.housecode], config.keycode[deviceObj.keycode], config.command[command]);
 			//SerialPort.write(deviceObj.housecode + " " + deviceObj.keycode + " " + config.command[command]);
 			var serialport = require("serialport");
 			var SerialPort = serialport.SerialPort;
 			console.log('=====>', 'serial port', config.portName);
 			var sp = new SerialPort(config.portName, {
-				baudrate: 9600,
-				parser: serialport.parsers.readline("\n")
+				baudrate: 9600
 			}, false);
+
 			sp.open(function(error) {
 				if (error) {
 					sp.close(function() {
 						console.log('error', error);
 					});
 				} else {
-					sp.write(config.housecode[deviceObj.housecode] + " " + config.keycode[deviceObj.keycode] + " " + config.command[command]);
-					sp.close(function() {
-						console.log('=====>', 'send serialport complete !');
+					var bufdata = config.housecode[deviceObj.housecode] + " " + config.keycode[deviceObj.keycode] + " " + config.command[command];
+					console.log('=====>', 'data',bufdata);
+					sp.write(bufdata, function(error){
+						if (error) {
+							sp.close(function() {
+								console.log('error', error);
+							});
+						} else {
+							sp.close(function() {
+								console.log('=====>', 'send serialport complete !');
+							});
+						}
 					});
 				}
 			});
