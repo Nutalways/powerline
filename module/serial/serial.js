fabef64cb63773016ee32ff3e5da2c2bc6579e20
Nutalways <nut_thanatcha@hotmail.co.th>
2016-03-24 00:40:38 +0700

    new 240316 1240

--- module/serial/serial.js
+++ module/serial/serial.js
@@ -1,47 +1,51 @@
-module.exports = function (db, config) {
+module.exports = function(db, config) {
 
-	var method = {};
-	var moment = require('moment');
+  var method = {};
+  var moment = require('moment');
 
 
-	method['send'] = function (deid, command) {
-		var deviceObj = db.device.getById(deid);
-		if (deviceObj && config.portName != "" && config.command[command] && config.housecode[deviceObj.housecode] && config.keycode[deviceObj.keycode]) {
-			console.log(moment.utc().toDate().getTime(), "=====>", "start send", deviceObj.id, config.housecode[deviceObj.housecode], config.keycode[deviceObj.keycode], config.command[command]);
-			//SerialPort.write(deviceObj.housecode + " " + deviceObj.keycode + " " + config.command[command]);
-			var serialport = require("serialport");
-			var SerialPort = serialport.SerialPort;
-			console.log('=====>', 'serial port', config.portName);
-			var sp = new SerialPort(config.portName, {
-				baudrate: 9600
-			}, false);
+  method['send'] = function(deid, command) {
+    var deviceObj = db.device.getById(deid);
+    if (deviceObj && config.portName != "" && config.command[command] && config.housecode[deviceObj.housecode] && config.keycode[deviceObj.keycode]) {
+      console.log(moment.utc().toDate().getTime(), "=====>", "start send", deviceObj.id, config.housecode[deviceObj.housecode], config.keycode[deviceObj.keycode], config.command[command]);
+      //SerialPort.write(deviceObj.housecode + " " + deviceObj.keycode + " " + config.command[command]);
+      var serialport = require("serialport");
+      var SerialPort = serialport.SerialPort;
+      console.log('=====>', 'serial port', config.portName);
+      var sp = new SerialPort(config.portName, {
+        baudrate: 9600,
+        dataBits: 8,
+        parity: 'none',
+        stopBits: 1,
+        flowControl: false
+      }, false);
 
-			sp.open(function(error) {
-				if (error) {
-					sp.close(function() {
-						console.log('error', error);
-					});
-				} else {
-					var bufdata = config.housecode[deviceObj.housecode] + " " + config.keycode[deviceObj.keycode] + " " + config.command[command];
-					console.log('=====>', 'data',bufdata);
-					sp.write(bufdata, function(error){
-						if (error) {
-							sp.close(function() {
-								console.log('error', error);
-							});
-						} else {
-							sp.close(function() {
-								console.log('=====>', 'send serialport complete !');
-							});
-						}
-					});
-				}
-			});
-		}else{
-			console.log('=====>', 'send serialport error !');
-		}
-	};
+      sp.open(function(error) {
+        if (error) {
+          sp.close(function() {
+            console.log('error', error);
+          });
+        } else {
+          var bufdata = config.housecode[deviceObj.housecode] + " " + config.keycode[deviceObj.keycode] + " " + config.command[command];
+          console.log('=====>', 'data', bufdata);
+          sp.write(bufdata, function(error) {
+            if (error) {
+              sp.close(function() {
+                console.log('error', error);
+              });
+            } else {
+              sp.close(function() {
+                console.log('=====>', 'send serialport complete !');
+              });
+            }
+          });
+        }
+      });
+    } else {
+      console.log('=====>', 'send serialport error !');
+    }
+  };
 
-	return method;
+  return method;
 
 };
