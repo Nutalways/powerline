dbebd4827a28ee42af19fa75efcc4dc717bae31a
Nutalways <nut_thanatcha@hotmail.co.th>
2016-03-23 20:40:44 +0700

    update

--- module/serial/serial.js
+++ module/serial/serial.js
@@ -6,7 +6,7 @@ module.exports = function (db, serial, config) {
 	method['send'] = function (deid, command) {
 		var deviceObj = db.device.getById(deid);
 		if (deviceObj && config.command[command] && config.housecode[deviceObj.housecode] && config.keycode[deviceObj.keycode]) {
-			serialPort.write(deviceObj.housecode + " " + deviceObj.keycode + " " + config.command[command]);
+			serial.write(deviceObj.housecode + " " + deviceObj.keycode + " " + config.command[command]);
 			console.log(moment.utc().toDate().getTime(), "=====>", deviceObj.id, deviceObj.housecode, deviceObj.keycode, command);
 		}
 	};
